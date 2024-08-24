aa1235f7e2ae72847c0310a3cc6d083e28a893bc
leboeuf <ml@maximeleboeuf.com>
2024-08-23 22:49:32 -0400

    Refactor out download method, more logging

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,7 +3,7 @@ import Photos
 import GRDB
 
 let dateFormatter = DateFormatter()
-dateFormatter.dateFormat = "yyyyMMdd HHmmss"
+dateFormatter.dateFormat = "yyyyMMdd_HHmmss"
  
 // Request authorization to access the photo library
 PHPhotoLibrary.requestAuthorization { status in
@@ -30,18 +30,43 @@ for sharedAlbum: PHAssetCollection in sharedAlbums {
     // Fetch assets
     let assets = fetchPhotosInAlbum(for: sharedAlbum)
     assets.enumerateObjects { (asset, index, stop) in
-        guard let assetId = extractGuidFromFilename(for: asset) else {
-            print("Error getting asset ID.")
-            exit(1)
-        }
-
-        print(assetId)
-        let creationDate = getCreationDate(of: asset, using: dateFormatter)
-
-        let customId = "\(creationDate) \(assetId)".trimmingCharacters(in: .whitespaces)
-        print("+ \(customId)")
-        exit(EXIT_SUCCESS)
+        print("[\(index + 1)/\(sharedAlbum.estimatedAssetCount)]")
+        downloadAsset(asset: asset, db: albumDb, albumId: albumId)
     }
 }
 
 exit(EXIT_SUCCESS)
+
+func downloadAsset(asset: PHAsset, db: DatabaseQueue, albumId: String) {
+    guard let assetId = extractGuidFromFilename(for: asset) else {
+        print("Error getting asset ID.")
+        exit(1)
+    }
+
+    let creationDate = getCreationDate(of: asset, using: dateFormatter)
+    let customId = "\(creationDate)_\(assetId)".trimmingCharacters(in: .whitespaces)
+    print("+ \(customId)")
+
+    let outputDirectory = getBackupDirectory()
+        .appendingPathComponent(albumId)
+
+    if (asset.mediaType == .video) {
+        // Request video
+        print("  + Video")
+        exit(EXIT_SUCCESS) // TODO
+        return
+    }
+
+    if (asset.mediaType == .image) {
+        // Request image
+        print("  + Image")
+
+        if (asset.mediaSubtypes.contains(.photoLive)) {
+            print("  + Live photo")
+        }
+        return
+    }
+
+    print("Error: unknown asset type. Aborting.")
+    exit(EXIT_SUCCESS)
+}
